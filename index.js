36b1d51291e8dbdef7bca371c158f3e2a2328584
Chrixmc17 <115506328+Chrixmc17@users.noreply.github.com>
2022-10-14 19:45:32 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const yestBtn = document.querySelector('#yesBtn');
+
+yestBtn.addEventListener('click',function () {
+    alert('Bueno, si tuviste un dia cansado recuerda que hoy hiciste tu mejor esfuerzo y claro que lo noté, eres una persona muy trabajadora y eso me alegra de ti<3')
+});
+const noBtn = document.querySelector('#noBtn');
+noBtn.addEventListener('click',function () {
+    alert('Losé, he visto con mis propios ojos tus grandes logros actualmente, se que estas cansada pero mira el lado bueno, tu lo lograste, lograste hacer un gran avanze en tu vida. Eres la mejor y de eso no lo dudo. Confía en tí y logra todas tus metas<3')
+})
